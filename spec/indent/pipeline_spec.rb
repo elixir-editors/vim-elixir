4ce7157c816d7541f61448fd3ccc4f727f13df97
Kassio Borges <kassioborgesm@gmail.com>
2013-09-19 14:50:36 -0300

    indent pipeline operator

---
+++ spec/indent/pipeline_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "Indenting" do
+  it "using multiline pipeline" do
+    assert_correct_indenting <<-EOF
+    "a,b,c,d"
+      |> String.split(",")
+      |> Enum.reverse
+    EOF
+  end
+
+  it "attribuition using multline pipeline operator" do
+    assert_correct_indenting <<-EOF
+    [ h | t ] = "a,b,c,d"
+      |> String.split(",")
+      |> Enum.reverse
+    EOF
+  end
+
+  it "function with pipeline operator" do
+    assert_correct_indenting <<-EOF
+    def test do
+      [ h | t ] = "a,b,c,d"
+        |> String.split(",")
+        |> Enum.reverse
+
+      { :ok, h }
+    end
+    EOF
+  end
+
+  it "pipeline operator with block open" do
+    assert_correct_indenting <<-EOF
+    def test do
+      "a,b,c,d"
+        |> String.split(",")
+        |> Enum.first
+        |> case do
+          "a" -> "A"
+          _ -> "Z"
+        end
+    end
+    EOF
+  end
+end
