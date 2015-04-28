c613687bdcfc54d84261f585eba291262a5d4f6a
Luper Rouch <luper.rouch@gmail.com>
2015-04-28 22:28:23 +0200

    Fix \ escaping in char list test

--- spec/syntax/list_spec.rb
+++ spec/syntax/list_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "List syntax" do
   it 'should properly handle "\\\\" inside' do
     syntax = <<-EOF
-      '"\\'
+      '"\\\\'
       var = 1
     EOF
     syntax.should include_elixir_syntax('elixirId', 'var')
