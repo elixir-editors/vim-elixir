be7cc1f63e193eb1a4ec246f9b336a856460515c
Kassio Borges <kassioborgesm@gmail.com>
2016-09-15 14:39:17 +0100

    Fix typo from the last commit :bomb:

--- spec/indent/lists_spec.rb
+++ spec/indent/lists_spec.rb
@@ -179,7 +179,7 @@ describe 'Indenting lists' do
   end
 
   describe 'mix of opened symbols' do
-    it 'indents every opened symbol' do
+    pending 'indents every opened symbol' do
       expect(<<~EOF).to be_elixir_indentation
       def test_another_feature do
         assert json_response(conn, 200) == %{
