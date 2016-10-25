b404447bcd8bdb291e780396e8d73a1c27f04213
Lee Marlow <lee.marlow@gmail.com>
2016-10-25 03:57:59 -0600

    Show @doc with sigil_S as elixirDocString. (#185)
    * Test to show @doc with sigil_S don't show as elixirDocString.
    * Make the tests pending until they're fixed.
    * Fix pending elixirDocString tests.
    * Keep elixirVariable for `@doc`.
    * Triple single quoted docs aren't valid unless with `sigil_[Ss]`.
    * Documentation delimiters are no longer included in `elixirDocString`.

--- spec/syntax/heredoc_spec.rb
+++ spec/syntax/heredoc_spec.rb
@@ -4,28 +4,56 @@ require 'spec_helper'
 
 describe 'Heredoc syntax' do
   describe 'binary' do
-    it 'with multiline content' do
-      expect(<<~EOF).to include_elixir_syntax('elixirDocString', 'foo')
-        @doc """
+    it 'doc with multiline content' do
+      ex = <<~EOF
+        @callbackdoc """
         foo
         """
       EOF
+      expect(ex).to include_elixir_syntax('elixirVariable', 'doc')
+      expect(ex).to include_elixir_syntax('elixirDocString', 'foo')
     end
 
-    it 'escapes quotes unless only preceded by whitespace' do
-      expect(<<~EOF).to include_elixir_syntax('elixirDocString', %q(^\s*\zs"""))
-        @doc """
-        foo """
+    it 'doc with sigil_S triple double-quoted multiline content' do
+      ex = <<~EOF
+        @doc ~S"""
+        foo
         """
       EOF
+      expect(ex).to include_elixir_syntax('elixirVariable', 'doc')
+      expect(ex).to include_elixir_syntax('elixirSigilDelimiter', 'S"""')
+      expect(ex).to include_elixir_syntax('elixirDocString', 'foo')
     end
 
-    it 'with interpolation' do
-      expect(<<~EOF).to include_elixir_syntax('elixirInterpolation', 'bar')
+    it 'doc with sigil_S triple single-quoted multiline content' do
+      ex = <<~EOF
+        @doc ~S'''
+        foo
+        '''
+      EOF
+      expect(ex).to include_elixir_syntax('elixirVariable', 'doc')
+      expect(ex).to include_elixir_syntax('elixirSigilDelimiter', "S'''")
+      expect(ex).to include_elixir_syntax('elixirDocString', 'foo')
+    end
+
+    it 'doc with triple single-quoted multiline content is not a doc string' do
+      ex = <<~EOF
+        @doc '''
+        foo
+        '''
+      EOF
+      expect(ex).not_to include_elixir_syntax('elixirDocString', 'foo')
+    end
+
+    it 'doc with interpolation' do
+      ex = <<~EOF
         @doc """
         foo \#{bar}
         """
       EOF
+      expect(ex).to include_elixir_syntax('elixirDocString', 'foo')
+      expect(ex).to include_elixir_syntax('elixirStringDelimiter', '"""')
+      expect(ex).to include_elixir_syntax('elixirInterpolation', 'bar')
     end
 
     it 'interpolation in heredoc must be string' do
