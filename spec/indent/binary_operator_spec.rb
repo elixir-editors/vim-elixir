59a6900436f28460ad8bb1193e2b783eb083183b
Josh Bodah <jb3689@yahoo.com>
2017-03-06 09:03:20 -0500

    add test case for #282

--- spec/indent/binary_operator_spec.rb
+++ spec/indent/binary_operator_spec.rb
@@ -13,4 +13,32 @@ describe 'Binary operators' do
 
   IO.puts word
   EOF
+
+  # TODO: @jbodah 2017-03-06: 209
+  # i <<~EOF
+  # def hello do
+  #   expected = "hello"
+  #              <> "world"
+  #   IO.puts expected
+  # end
+  # EOF
+
+  i <<~EOF
+  def hello do
+    expected =
+      "hello"
+      <> "world"
+    IO.puts expected
+  end
+  EOF
+
+  i <<~EOF
+  alias Rumbl.Repo
+  alias Rumbl.Category
+
+  for category <- ~w(Action Drama Romance Comedy Sci-fi) do
+    Repo.get_by(Category, name: category) ||
+      Repo.insert!(%Category{name: category})
+  end
+  EOF
 end
