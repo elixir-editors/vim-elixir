983bf77b18b6a3e0ba7386098c15858473389976
Josh Bodah <jb3689@yahoo.com>
2017-03-27 21:52:44 -0400

    indent: add handle_starts_with_comment

--- spec/indent/comment_spec.rb
+++ spec/indent/comment_spec.rb
@@ -9,5 +9,33 @@ describe 'Indenting *after* comments' do
     IO.puts :test
     EOF
   end
+
+  it 'aligns comments with pipes' do
+    expect(<<~EOF).to be_elixir_indentation
+    defmodule Foo do
+      def run do
+        list =
+          File.read!("/path/to/file")
+          |> String.split()
+          # now start a new line
+          # used to start here
+          # but now starts here
+      end
+    end
+    EOF
+  end
+
+  it 'aligns comments after guard clause func' do
+    expect(<<~EOF).to be_elixir_indentation
+    defmodule Foo do
+      def run(task) when task in [:t1, :t2] do
+      end
+
+      # now starts a new line
+      # use to start here
+      # but now starts here
+    end
+    EOF
+  end
 end
 
