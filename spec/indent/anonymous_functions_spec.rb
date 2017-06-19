28fa1c2a00a709f850a7bcc8308cef8184056111
Josh Bodah <jb3689@yahoo.com>
2017-06-19 08:58:50 -0400

    drop support for weird function test case

--- spec/indent/anonymous_functions_spec.rb
+++ spec/indent/anonymous_functions_spec.rb
@@ -61,17 +61,6 @@ describe 'Indenting anonymous functions' do
     defp handle_chunk(:err, line, state) do
       update_in(state[:stderr],
                 fn
-                ->
-                ->
-                end)
-      Map.update(state, :stderr, [line], &(&1 ++ [line]))
-    end
-  EOF
-
-  i <<~EOF
-    defp handle_chunk(:err, line, state) do
-      update_in(state[:stderr],
-                fn
                   hello -> :ok
                   world -> :ok
                 end)
