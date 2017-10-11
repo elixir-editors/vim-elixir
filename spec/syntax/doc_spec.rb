838f93c225b25f6b750cf0ad8bff75a801ae4aa7
Marco Hinz <mh.codebro@gmail.com>
2017-10-11 23:27:25 +0200

    spec: support interpolation in docstrings

--- spec/syntax/doc_spec.rb
+++ spec/syntax/doc_spec.rb
@@ -61,9 +61,9 @@ describe 'documentation syntax' do
         foo #{bar}
         """
       EOF
-      expect(ex).to     include_elixir_syntax('elixirDocString',       'foo')
-      expect(ex).to     include_elixir_syntax('elixirStringDelimiter', '"""')
-      expect(ex).not_to include_elixir_syntax('elixirInterpolation',   'bar')
+      expect(ex).to include_elixir_syntax('elixirDocString',       'foo')
+      expect(ex).to include_elixir_syntax('elixirStringDelimiter', '"""')
+      expect(ex).to include_elixir_syntax('elixirInterpolation',   'bar')
     end
 
     it 'doc with doctest' do
