cad0371ad6affcead542da9333f4bc37ff0ac8d6
Brian Gomes Bascoy <pera@users.noreply.github.com>
2017-11-10 18:44:53 -0800

    Update manual_install.sh
    In the previous version if ~/.vim exists and is already populated, directories would be copied incorrectly, resulting in something like ~/.vim/syntax/syntax/, ~/.vim/indent/indent, etc.

--- manual_install.sh
+++ manual_install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-cp -R autoload/ ~/.vim/autoload/ &&
-    cp -R compiler/ ~/.vim/compiler/ &&
-    cp -R ftdetect/ ~/.vim/ftdetect/ &&
-    cp -R ftplugin/ ~/.vim/ftplugin/ &&
-    cp -R indent/ ~/.vim/indent/ &&
-    cp -R syntax/ ~/.vim/syntax/
+mkdir -p ~/.vim
+cp -R autoload/ compiler/ ftdetect/ ftplugin/ indent/ syntax/ ~/.vim/
