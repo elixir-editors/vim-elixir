2fc3b481aacd0aecfa732240806186a44ce8255a
joel hansson <joel@stoer.se>
2018-06-30 21:14:08 +0200

    manual_install: Safer copy to target directories

--- manual_install.sh
+++ manual_install.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -eu
 
 for INSTALL_DIR in autoload compiler ftdetect ftplugin indent syntax
 do
   mkdir -p ~/.vim/${INSTALL_DIR}
-  cp -R ${INSTALL_DIR}/ ~/.vim/${INSTALL_DIR}
+  (set -x
+  cp -R ${INSTALL_DIR}/* ~/.vim/${INSTALL_DIR}/)
 done
