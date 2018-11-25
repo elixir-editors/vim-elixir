12c7d514e506b402b9c6bae460675f053b084f0a
Thomas Stratmann <thomas.stratmann@9elements.com>
2018-11-25 13:50:29 +0100

    add a few examples

--- spec/folding/basic_spec.rb
+++ spec/folding/basic_spec.rb
@@ -12,6 +12,47 @@ describe 'Basic folding' do
   it_folds_lines(<<~EOF, 2)
   defmodule M do
   end
+  "not in fold"
+  EOF
+
+  it_folds_lines(<<~EOF, 4)
+  defmodule M do
+    def some_func do
+    end
+  end
+  "not in fold"
+  EOF
+
+  it_folds_lines(<<~EOF, 2, on_line: 2)
+  defmodule M do
+    def some_func do
+    end
+  end
+  "not in fold"
+  EOF
+
+  it_folds_lines(<<~EOF, 2)
+  if true do
+  end
+  "not in fold"
+  EOF
+
+  it_folds_lines(<<~EOF, 3, on_line: 3)
+  if true do
+    nil
+  else
+    nil
+  end
+  "not in fold"
+  EOF
+
+  it_folds_lines(<<~EOF, 5, skip: "broken")
+  if true do
+    nil
+  else
+    nil
+  end
+  "not in fold"
   EOF
 end
 
