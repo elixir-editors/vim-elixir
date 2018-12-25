ccfb641fad10682846ba4c1cd7927d6239648291
Alexander Veit <alexander.veit@gmx.net>
2018-12-25 01:05:38 +0000

    Fix issue-459
    Issue 459: manual_install.sh creates wrong directory hierarchy.

--- manual_install.sh
+++ manual_install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+mkdir -p ~/.vim/
+
 for INSTALL_DIR in autoload compiler ftdetect ftplugin indent syntax
 do
-  mkdir -p ~/.vim/${INSTALL_DIR}
-  cp -R ${INSTALL_DIR}/ ~/.vim/${INSTALL_DIR}
+  cp -R ${INSTALL_DIR} ~/.vim/
 done
+
