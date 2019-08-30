9f5b45b93dc6fd6a53c8f344bd8c9dccbfb6c1a2
William Sanches <william.sanches@plataformatec.com.br>
2019-08-29 20:49:41 -0400

    Add more tests to the suite

--- spec/syntax/function_spec.rb
+++ spec/syntax/function_spec.rb
@@ -18,15 +18,57 @@ describe 'function syntax' do
     EOF
   end
 
+  it 'detects higher order function calls' do
+    expect(<<~'EOF').to include_elixir_syntax('elixirFunctionCall', 'func')
+      func.()
+    EOF
+  end
+
   it 'detects function calls with parenthesis' do
     expect(<<~'EOF').to include_elixir_syntax('elixirFunctionCall', 'func')
       func()
     EOF
   end
 
+  it 'detects function calls with bangs' do
+    expect(<<~'EOF').to include_elixir_syntax('elixirFunctionCall', 'func!')
+      func!()
+    EOF
+  end
+
+  it 'detects function calls with question marks' do
+    expect(<<~'EOF').to include_elixir_syntax('elixirFunctionCall', 'func?')
+      func?()
+    EOF
+  end
+
+  it 'detects function calls appended by module with parenthesis' do
+    expect(<<~'EOF').to include_elixir_syntax('elixirFunctionCall', 'func')
+      Mod.func()
+    EOF
+  end
+
   it 'detects function calls appended by module without parenthesis' do
     expect(<<~'EOF').to include_elixir_syntax('elixirFunctionCall', 'func')
       Mod.func
+    EOF
+  end
+
+  it 'does not highlight terms without a parentheresis' do
+    expect(<<~'EOF').not_to include_elixir_syntax('elixirFunctionCall', 'func')
+      func
+    EOF
+  end
+
+  it 'does not detect calls to function with invalid names' do
+    expect(<<~'EOF').not_to include_elixir_syntax('elixirFunctionCall', '2fast2func')
+      2fast2func()
+    EOF
+  end
+
+  it 'ignores spacing between module and function names' do
+    expect(<<~'EOF').not_to include_elixir_syntax('elixirFunctionCall', 'func')
+      Module .           func
     EOF
   end
 end
