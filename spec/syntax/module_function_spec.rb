4b5958c4e92986a248ba97cd4083b43431859ace
Josh Bodah <joshuabodah@gmail.com>
2020-03-10 20:45:02 -0400

    add elixirFunctionCall
    Implementation from https://github.com/elixir-editors/vim-elixir/pull/506
    
    cc: Thanks @w-sanches

--- spec/syntax/module_function_spec.rb
+++ spec/syntax/module_function_spec.rb
@@ -4,13 +4,13 @@ require 'spec_helper'
 
 describe 'Module function syntax' do
   it 'for used as module function' do
-    expect(<<~EOF).to include_elixir_syntax('elixirId', 'for')
+    expect(<<~EOF).to include_elixir_syntax('elixirFunctionCall', 'for')
     OverridesDefault.for
     EOF
   end
 
   it 'case used as module function' do
-    expect(<<~EOF).to include_elixir_syntax('elixirId', 'case')
+    expect(<<~EOF).to include_elixir_syntax('elixirFunctionCall', 'case')
     OverridesDefault.case
     EOF
   end
